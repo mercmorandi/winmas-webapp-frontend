8ce3df113f962211c952d7574cb74684dfd2c044
Mauro Bellinazzi <mauro.bellinazzi@ovalmoney.com>
2020-07-21 20:28:15 +0200

    added environmens and get_stats in line.service

--- angular/winmas/src/app/line.service.ts
+++ angular/winmas/src/app/line.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment'
+import { Observable } from 'rxjs';
+import { lineStatVM } from './models/line-stat-vm';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LineService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  get_stats(): Observable<lineStatVM[]> {
+    return this.httpClient.get<lineStatVM[]>(`${environment.apiUrl}/get_stats`)
+      .pipe(tap((data) => {
+      console.log('get_stats', data);
+    }));
+  }
 }
---
+++ angular/winmas/src/app/models/line-stat-vm.ts
@@ -0,0 +1,4 @@
+export class lineStatVM {
+  nDevices: Number;
+  minute: String;
+}
