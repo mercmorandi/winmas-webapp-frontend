3ab6442fb2c9da8531c07a68b65ce70b394ca878
mrk-stss <merkmorad@gmail.com>
2020-09-13 18:46:06 +0200

    minor fix

--- angular/winmas/src/app/app.module.ts
+++ angular/winmas/src/app/app.module.ts
@@ -74,7 +74,7 @@ const appRoutes: Routes = [
     ScatterDetailsComponent,
     HistogramChartComponent,
     HistogramDashboardComponent,
-    HistogramConfigComponent
+    HistogramConfigComponent,
     ProxyDashboardComponent,
     ProxyConfigComponent,
     ProxyStatusComponent,
